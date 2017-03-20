9631a466bfd67e7a2445effd010553cb83a21cec
Vadim St <vadim_st_13@mail.ru>
2017-03-20 15:22:13 +0300

    fix fonts for all clients

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var gulp = require('gulp');
 var less = require('gulp-less');
 var plumber = require("gulp-plumber");
@@ -7,6 +9,10 @@ var server = require("browser-sync");
 var rename = require('gulp-rename');
 var svgstore = require('gulp-svgstore');
 var svgmin = require('gulp-svgmin');
+var imagemin = require('gulp-imagemin');
+var mqpacker = require('css-mqpacker');
+var minify = require('gulp-csso');
+var run = require('run-sequence');
 
 gulp.task('style', function() {
 	gulp.src('less/style.less')
@@ -37,6 +43,30 @@ gulp.task('server', ["style"], function() {
 
 });
 
+gulp.task('stylefinal', function() {
+	gulp.src('less/style.less')
+		.pipe(plumber())
+		.pipe(less())
+		.pipe(postcss([
+			autoprefixer({browsers: [
+				"last 1 version",
+				"last 2 Chrome version",
+				"last 2 Firefox version",
+				"last 2 Opera version",
+				"last 2 Edge version"
+			]}),
+			mqpacker({
+				sort: true
+			})
+		]))
+		.pipe(gulp.dest('css'))
+		.pipe(minify())
+		.pipe(rename('style.min.css'))
+		.pipe(gulp.dest('css'));
+		
+		
+});
+
 gulp.task('symbols', function() {
 	return gulp.src('img/icon/*.svg')
 		.pipe(svgmin())
@@ -45,4 +75,17 @@ gulp.task('symbols', function() {
 		}))
 		.pipe(rename('symbols.svg'))
 		.pipe(gulp.dest('img'));
+});
+
+gulp.task('images', function() {
+	return gulp.src('img/**/*.{png,jpg,gif}')
+		.pipe(imagemin([
+			imagemin.optipng({optimizationLevel: 3}),
+			imagemin.jpegtran({progressive: true})
+		]))
+		.pipe(gulp.dest('img'));
+});
+
+gulp.task('build', function(fn) {
+	run('stylefinal', 'images', 'symbols', fn);
 });
